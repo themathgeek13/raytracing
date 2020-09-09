5e8b1148be2410ccb532fd559e4dbebc5b07efbb
rohan <rgrao@andrew.cmu.edu>
2020-09-09 16:26:12 -0400

    added a vec3 library header

---
+++ cmake-build-debug/CMakeFiles/raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracer.dir/raytracer.cpp.o"
+  "raytracer"
+  "raytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
