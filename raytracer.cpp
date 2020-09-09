d0374a2c79ead1c7ed04a64b9e94a60567795a96
rohan <rgrao@andrew.cmu.edu>
2020-09-09 18:31:39 -0400

    working on sphere model and tracing rays

--- raytracer.cpp
+++ raytracer.cpp
@@ -2,9 +2,29 @@
 #include "vec3.h"
 #include "ray.h"
 
+float hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else
+    {
+        return (-b-sqrt(discriminant) / (2.0*a));
+    }
+}
+
 vec3 color(const ray& r) {
+    float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+    if(t > 0.0) {
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*vec3(1.0,1.0,1.0)+t*vec3(0.5,0.7,1.0);
 }
 
